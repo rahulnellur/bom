1af95050607edd6f61b1c06a738ecfb9571b2ea4
Rahul Kv <rahulkv@Mac-mini.local>
2025-05-08 10:42:53 +0530

    Add setup.py to make app valid for Frappe Cloud

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bom',
+    version='0.0.1',
+    author='rahulnellur',
+    author_email='rahul@rahulkv.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
